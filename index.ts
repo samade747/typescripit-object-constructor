c18b024501cd605c04f3a03a3fc10b032cb81603
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-06-08 19:28:27 +0500

    Update index.ts5

--- index.ts
+++ index.ts
@@ -1,67 +1,163 @@
 // // object constructor
 
-// // class
-// class House {
-//     numberOfDoors: number 
-//     numberOfWindows: number 
-//     houseNumber: number  
-//     ownerName: string 
+// class
+class House {
+    numberOfDoors: number 
+    numberOfWindows: number 
+    houseNumber: number  
+    ownerName: string 
 
-// // constructor
-// // constructor is a function that is called when an object is created
-// constructor(numberOfDoors: number, numberOfWindows: number, houseNumber: number, ownerName: string) {
-//     this.numberOfDoors = numberOfDoors
-//     this.numberOfWindows = numberOfWindows
-//     this.houseNumber = houseNumber
-//     this.ownerName = ownerName
+// constructor
+// constructor is a function that is called when an object is created
+constructor(numberOfDoors: number, numberOfWindows: number, houseNumber: number, ownerName: string) {
+    this.numberOfDoors = numberOfDoors
+    this.numberOfWindows = numberOfWindows
+    this.houseNumber = houseNumber
+    this.ownerName = ownerName
+}
+
+}
+
+
+let house = new House(3, 5, 12, "John")
+let house2 = new House(3, 5, 12, "Jsamad")
+
+
+console.log(house2)
+
+
+
+
+// class Animal {
+//     name : string
+
+//     constructor(name: string){
+//         this.name = name
+
+//     }
+//     walk() {
+//         console.log("walking")    
+//     }
+
+//     eat(){
+//         console.log("eating")    
+//     }
+    
 // }
 
+// class Dog extends Animal {
+//     constructor(name: string){
+//         super(name)
+//     }
+
+// }
+
+// let dl = new Dog("dog")
+
+
+// class Cat {
+//     meow(){
+//         console.log("meow meow ")
+//     }
 // }
 
 
-// let house = new House(3, 5, 12, "John")
-// let house2 = new House(3, 5, 12, "Jsamad")
 
 
-// console.log(house2)
+
+<<<<<<< HEAD
+// class Animal {
+//     name : string
+
+//     constructor(name: string){
+//         this.name = name
+
+//     }
+//     walk() {
+//         console.log("walking")    
+//     }
+
+//     eat(){
+//         console.log("eating")    
+//     }
+    
+// }
+
+// class Dog extends Animal {
+//     constructor(name: string){
+//         super(name)
+//     }
+
+// }
+
+// let dl = new Dog("dog")
+
+
+// class Cat {
+//     meow(){
+//         console.log("meow meow ")
+//     }
+// }
+
 
 
 
 
 class Animal {
-    name : string
+    name: string;
 
-    constructor(name: string){
-        this.name = name
-
+    constructor(name: string) {
+        this.name = name;
     }
+
     walk() {
-        console.log("walking")    
+        console.log(`${this.name} is walking`);
     }
 
-    eat(){
-        console.log("eating")    
+=======
+class Animal {
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
     }
-    
+
+    walk() {
+        console.log(`${this.name} is walking`);
+    }
+
+>>>>>>> 6b96373d2c68666bf42cfea4746418f446c16724
+    eat() {
+        console.log(`${this.name} is eating`);
+    }
 }
 
 class Dog extends Animal {
-    constructor(name: string){
-        super(name)
+    constructor(name: string) {
+        super(name);
     }
 
-}
-
-let dl = new Dog("dog")
-
-
-class Cat {
-    meow(){
-        console.log("meow meow ")
+    bark() {
+        console.log(`${this.name} is barking`);
     }
 }
 
+let dl = new Dog("Dog");
+dl.walk(); // Dog is walking
+dl.eat();  // Dog is eating
+dl.bark(); // Dog is barking
 
+class Cat extends Animal {
+    constructor(name: string) {
+        super(name);
+    }
 
+    meow() {
+        console.log(`${this.name} says meow meow`);
+    }
+}
 
-
+let cl = new Cat("Cat");
+cl.walk(); // Cat is walking
+cl.eat();  // Cat is eating
+cl.meow(); // Cat says meow meow
