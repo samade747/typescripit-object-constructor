fa5ac92779c9340ad9305a04840426b23bb98dd1
samade747 <samad.e747@gmail.com>
2024-06-08 09:31:01 +0500

    930am 6 8 2024

---
+++ index.js
@@ -0,0 +1,11 @@
+var House = /** @class */ (function () {
+    function House() {
+        this.numberOfDoors = 2;
+        this.numberOfWindows = 4;
+        this.houseNumber = 1;
+        this.ownerName = "John Doe";
+    }
+    return House;
+}());
+var h1 = new House();
+console.log(h1);
---
+++ index.ts
@@ -0,0 +1,15 @@
+class House {
+    numberOfDoors: number = 2;
+    numberOfWindows: number = 4;
+    houseNumber: number = 1;
+    ownerName: string = "John Doe";
+    
+    
+}
+
+
+
+
+let h1 = new House();
+
+console.log(h1)
