6b96373d2c68666bf42cfea4746418f446c16724
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-06-08 19:25:30 +0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,29 +1,29 @@
 // // object constructor
 
-// // class
-// class House {
-//     numberOfDoors: number 
-//     numberOfWindows: number 
-//     houseNumber: number  
-//     ownerName: string 
+// class
+class House {
+    numberOfDoors: number 
+    numberOfWindows: number 
+    houseNumber: number  
+    ownerName: string 
 
-// // constructor
-// // constructor is a function that is called when an object is created
-// constructor(numberOfDoors: number, numberOfWindows: number, houseNumber: number, ownerName: string) {
-//     this.numberOfDoors = numberOfDoors
-//     this.numberOfWindows = numberOfWindows
-//     this.houseNumber = houseNumber
-//     this.ownerName = ownerName
-// }
+// constructor
+// constructor is a function that is called when an object is created
+constructor(numberOfDoors: number, numberOfWindows: number, houseNumber: number, ownerName: string) {
+    this.numberOfDoors = numberOfDoors
+    this.numberOfWindows = numberOfWindows
+    this.houseNumber = houseNumber
+    this.ownerName = ownerName
+}
 
-// }
-
-
-// let house = new House(3, 5, 12, "John")
-// let house2 = new House(3, 5, 12, "Jsamad")
+}
 
 
-// console.log(house2)
+let house = new House(3, 5, 12, "John")
+let house2 = new House(3, 5, 12, "Jsamad")
+
+
+console.log(house2)
 
 
 
