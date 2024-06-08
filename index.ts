687632d1f6e4846edd3cae7fd19de59bd1f0fce0
samade747 <samad.e747@gmail.com>
2024-06-08 11:43:23 +0500

    1143

--- index.ts
+++ index.ts
@@ -1,29 +1,66 @@
-// object constructor
+// // object constructor
 
-// class
-class House {
-    numberOfDoors: number 
-    numberOfWindows: number 
-    houseNumber: number  
-    ownerName: string 
+// // class
+// class House {
+//     numberOfDoors: number 
+//     numberOfWindows: number 
+//     houseNumber: number  
+//     ownerName: string 
 
-// constructor
-// constructor is a function that is called when an object is created
-constructor(numberOfDoors: number, numberOfWindows: number, houseNumber: number, ownerName: string) {
-    this.numberOfDoors = numberOfDoors
-    this.numberOfWindows = numberOfWindows
-    this.houseNumber = houseNumber
-    this.ownerName = ownerName
+// // constructor
+// // constructor is a function that is called when an object is created
+// constructor(numberOfDoors: number, numberOfWindows: number, houseNumber: number, ownerName: string) {
+//     this.numberOfDoors = numberOfDoors
+//     this.numberOfWindows = numberOfWindows
+//     this.houseNumber = houseNumber
+//     this.ownerName = ownerName
+// }
+
+// }
+
+
+// let house = new House(3, 5, 12, "John")
+// let house2 = new House(3, 5, 12, "Jsamad")
+
+
+// console.log(house2)
+
+
+
+
+class Animal {
+    name : string
+
+    constructor(name: string){
+        this.name = name
+
+    }
+    walk() {
+        console.log("walking")    
+    }
+
+    eat(){
+        console.log("eating")    
+    }
+    
 }
 
+class Dog extends Animal {
+    constructor(name: string){
+        super(name)
+    }
+
 }
 
-
-let house = new House(3, 5, 12, "John")
-let house2 = new House(3, 5, 12, "Jsamad")
+let dl = new Dog("dog")
 
 
-console.log(house2)
+class Cat {
+    meow(){
+        console.log("meow meow ")
+    }
+}
+
 
 
 
